32461ffa84979f10432cc29974aef376f3f53dfb
周毅 <nihaozhouyi@163.com>
2018-07-06 01:32:01 +0800

    修改，运行

--- src/main/java/com/mmm/develop/user/dao/UserDao.java
+++ src/main/java/com/mmm/develop/user/dao/UserDao.java
@@ -4,4 +4,5 @@ import com.mmm.develop.common.dao.BaseDao;
 import com.mmm.develop.user.entity.User;
 
 public interface UserDao extends BaseDao<User>{
+
 }
--- src/main/java/com/mmm/develop/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmm/develop/user/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mmm.develop.user.service.impl;
 
+import java.util.List;
 import java.util.Map;
 
 import com.mmm.develop.user.dao.UserDao;
@@ -18,5 +19,30 @@ public class UserServiceImpl implements UserService {
     @Override
     public User get(Map<String, Object> paramMap) {
         return userDao.findOne(paramMap);
+    }
+
+    @Override
+    public int insertOne(User user) {
+        return 0;
+    }
+
+    @Override
+    public int updateOne(User user) {
+        return 0;
+    }
+
+    @Override
+    public int deleteOne(User user) {
+        return 0;
+    }
+
+    @Override
+    public User findOne(Map<String, Object> paramMap) {
+        return null;
+    }
+
+    @Override
+    public List<User> findMulti(Map<String, Object> paramMap) {
+        return null;
     }
 }
