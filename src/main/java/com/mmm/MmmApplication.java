3a3735ae0f8b6ef526d77c2a652310c2e91def79
lbj <532413861@qq.com>
2018-08-02 16:49:39 +0800

    websocket

--- src/main/java/com/mmm/MmmApplication.java
+++ src/main/java/com/mmm/MmmApplication.java
@@ -2,12 +2,19 @@ package com.mmm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @RestController
 @SpringBootApplication
 public class MmmApplication {
     public static void main(String[] args) {
         SpringApplication.run(MmmApplication.class, args);
+    }
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 }
